738efffe4e1c544873ebd557c3e20b56230f2a5c
Umakanta Patro <umakanta.patro@ti.com>
2012-09-11 14:39:40 +0530

    Add FileManager app to build
    Change-Id: Idd625a789e9eb5245877c5b056ef0dd8d9f8253b
    Signed-off-by: Umakanta Patro <umakanta.patro@ti.com>

--- device.mk
+++ device.mk
@@ -63,5 +63,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	lights.beaglebone
 
+PRODUCT_PACKAGES += \
+	FileManager-1.1.6
+
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, external/tslib/tslib.mk)
