pierluigi-passaro/rowboat-vendor-ti-am335xevm

Add ppp and usbmodswitch scripts
Change-Id: I0f53c740a87b63d36af6c42b97e28407d3ea7cd8
Signed-off-by: Pankaj Bharadiya <pankaj.bharadiya@ti.com>

device.mk
@@ -49,6 +49,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	device/ti/am335xevm/apns-full-conf.xml:system/etc/apns-conf.xml
 
+PRODUCT_COPY_FILES += \
+	device/ti/am335xevm/ppp/ip-up-datakey:system/etc/ppp/ip-up-datakey \
+	device/ti/am335xevm/ppp/ip-down-datakey:system/etc/ppp/ip-down-datakey \
+	device/ti/am335xevm/usbmodeswitch.conf:system/etc/usbmodeswitch.conf
+
 # KeyPads
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \

bhat10000/vishveshwars-beaglebone

Change-Id: I0204aad153b0d790e16e1fffced6532ce8f6acfa
Signed-off-by: Vishveshwar Bhat <vishveshwar.bhat@ti.com>

device.mk
@@ -26,6 +26,11 @@ PRODUCT_COPY_FILES := \
 PRODUCT_COPY_FILES += \
 	device/ti/beaglebone/apns-full-conf.xml:system/etc/apns-conf.xml
 
+PRODUCT_COPY_FILES += \
+	device/ti/beaglebone/ppp/ip-up-datakey:system/etc/ppp/ip-up-datakey \
+	device/ti/beaglebone/ppp/ip-down-datakey:system/etc/ppp/ip-down-datakey \
+	device/ti/beaglebone/usbmodeswitch.conf:system/etc/usbmodeswitch.conf
+
 # KeyPads
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \