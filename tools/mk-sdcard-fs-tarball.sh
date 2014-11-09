462db444333ecdc6ecdceb6ad4262640c977953c
Vishveshwar <vishveshwarbhat@aim.com>
2014-11-09 22:24:25 +0530

    tools: add conditionals in mk-sdcard-fs-tarball script
    This script supports only the beaglebone product and
    should not be run anywhere else as-is.
    
    Signed-off-by: Vishveshwar <vishveshwarbhat@aim.com>

--- tools/mk-sdcard-fs-tarball.sh
+++ tools/mk-sdcard-fs-tarball.sh
@@ -6,12 +6,32 @@
 
 # Based on rowboat top-level Makefile
 
+# first check if lunch is done
+if [ -z "$ANDROID_BUILD_TOP" ]; then
+    echo "ANDROID_BUILD_TOP undefined"
+exit 1
+fi
+if [ -z "$ANDROID_PRODUCT_OUT" ]; then
+    echo "ANDROID_PRODUCT_OUT undefined"
+exit 1
+fi
+
+# Check for beaglebone target
+if [ "$TARGET_PRODUCT" != "beaglebone" ]; then
+    echo "Only 'TARGET_PRODUCT = beaglebone' is supported for generating rootfs tarball"
+exit 1
+fi
+
 export ANDROID_FS_DIR=$ANDROID_PRODUCT_OUT/android_rootfs
 
-# TODO: Add conditional
 echo "ANDROID_FS_DIR: $ANDROID_FS_DIR"
-rm -rf $ANDROID_FS_DIR
-rm $ANDROID_BUILD_TOP/rootfs.tar.bz2
+if [ -d "$ANDROID_FS_DIR" ]; then
+    echo "Deleting old fs tarball"
+    rm -rf $ANDROID_FS_DIR
+fi
+if [ -e "$ANDROID_BUILD_TOP/rootfs.tar.bz2" ]; then
+    rm $ANDROID_BUILD_TOP/rootfs.tar.bz2
+fi
 mkdir $ANDROID_FS_DIR
 
 echo "Copying files..."
